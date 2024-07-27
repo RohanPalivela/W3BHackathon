550a444283e9337f98ed44b216f2a8967024db88
RohanPalivela <rohan.palivela@gmail.com>
2024-07-27 15:00:45 -0500

    Fixed api calls and button working !

--- src/app.py
+++ src/app.py
@@ -24,9 +24,7 @@ def queryWeather():
     lat = request.args.get('lat') 
     # Get weather data
     weather_data = get_weather_data(lat, long)
-    return jsonify(
-        power=predict_power(weather_data)
-    )
+    return weather_data
 
 # Function to get weather data based on latitude and longitude
 def get_weather_data(lat, lon):
@@ -50,28 +48,23 @@ def get_weather_data(lat, lon):
     return weather_data
 
 # Function to get weather data based on temperature, humidity, wind speed, and wind direction
-def get_weather_data_inputs(temperature, humidity, windspeed, winddirection):
-    # OpenWeatherMap API endpoint for current weather data using coordinates
-    url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric"
-
-    # Make a GET request to fetch the raw weather data
-    response = requests.get(url)
-    data = response.json()
-    if response.status_code == 200:
-        # Extract required data
-        weather_data = {
-            "temperature_2m": c_to_f(temperature),            # Actual air temperature
-            "relativehumidity_2m": humidity,      # Relative Humidity
-            "windspeed_100m": windspeed,                # Wind Speed
-            "winddirection_100m": winddirection              # Wind Direction
-        }
-    else:
-        weather_data = {"Error": data.get("message", "Unable to fetch data")}
-    
-    return weather_data
+@app.route('/findPower')
+def get_weather_data_inputs():
+    weather_data = {
+        "temperature_2m": c_to_f(request.args.get('temp')),            # Actual air temperature
+        "relativehumidity_2m": request.args.get('hum'),      # Relative Humidity
+        "windspeed_100m": request.args.get('speed'),                # Wind Speed
+        "winddirection_100m": request.args.get('dir')              # Wind Direction
+    }
+    print(request.args.get('temp'), request.args.get('hum'), request.args.get('speed'), request.args.get('dir') )
+    numb = predict_power(weather_data)
+    print(numb)
+    return jsonify(
+        power=numb
+    )
 
 def c_to_f(c):
-    return (((float) (c)) * (9/5) + 32)
+    return round((((float) (c)) * (9/5) + 32),2)
 
 def predict_power(weather_data):
     global model
--- src/static/main.js
+++ src/static/main.js
@@ -16,35 +16,65 @@ document.addEventListener('DOMContentLoaded', function() {
     let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
     map.addLayer(layer);
 
+    let btn = document.getElementsByClassName('button-green')[0];
+
+    let tempIn = document.getElementById('temperature');
+    let humidityIn = document.getElementById('humidity');
+    let windSpeedIn = document.getElementById('wind-speed');
+    let windDirIn = document.getElementById('wind-direction');
+    
+    btn.addEventListener("click", function() {
+
+      fetch("/findPower?temp="+tempIn.value+"&hum="+humidityIn.value+"&speed="+windSpeedIn.value+"&dir="+windDirIn.value)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        console.log("Working");
+        return response.json();
+      })
+      .then(result => {
+        console.log(result['power']);
+          let finalVal = document.getElementById('finalVal');
+          finalVal.innerHTML = (result['power'] * 100).toFixed(2)+"%";
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    }, false);
+
     let marker = null;
     map.on('click', (event)=> {
-    
-        if(marker !== null){
-            map.removeLayer(marker);
-        }
-    
-        marker = L.marker([event.latlng.lat , event.latlng.lng]).addTo(map);
+      
+      // REMOVING MARKER 
+      if(marker !== null){
+          map.removeLayer(marker);
+      }
+      
+      // SETTING DOWN MARKER
+      marker = L.marker([event.latlng.lat , event.latlng.lng]).addTo(map);
 
-        let latitude = event.latlng.lat;
-        let longitude = ((event.latlng.lng + 180) % 360) - 180;
-        document.getElementById('latitude').value = latitude;
-        document.getElementById('longitude').value = longitude;
-        
-        var data = fetch("/getWeatherData?long="+longitude+"&lat="+latitude)
-        .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            console.log("Working");
-            return response.json();
-          })
-          .then(data => {
-            console.log((data['power'] * 100).toFixed(2));
-            var finalVal = document.getElementById('finalVal');
-            finalVal.innerHTML = (data['power'] * 100).toFixed(2)+"%";
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
+      // SET LATITUDE AND LONGITUDE INPUTS
+      let latitude = event.latlng.lat;
+      let longitude = ((event.latlng.lng + 180) % 360) - 180;
+      document.getElementById('latitude').value = latitude;
+      document.getElementById('longitude').value = longitude;
+      
+      // UPDATE LIVE DATA 4 INPUTS
+      var data = fetch("/getWeatherData?long="+longitude+"&lat="+latitude)
+      .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          console.log("Working");
+          return response.json();
+        }).then(response => {
+          tempIn.value = response['temperature_2m'];
+          humidityIn.value = response['relativehumidity_2m'];
+          windSpeedIn.value = response['windspeed_100m'];
+          windDirIn.value = response['winddirection_100m'];
+        }).catch(error => {
+          console.error('Error:', error);
+        });
     })
 });
